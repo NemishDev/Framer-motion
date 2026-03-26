192d7c83fe29a8375c76309558147027e46f279c
Nemish Mogra <mogranemish@gmail.com>
2026-03-26 16:30:54 +0530

    pushing limits of animation with framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,29 @@
-import { motion ,useScroll } from "framer-motion";
-import React from "react"
+import { div } from 'framer-motion/client'
+import {AnimatePresence, motion} from 'framer-motion'
+import React, { useState } from 'react'
 
-const App = () => {
+const App =() =>{
+  const[btnClicked, setBtnClicked] = useState(false)
 
-   const {scrollYProgress}= useScroll()
-   console.log(scrollYProgress);
-  return (
-    <div>
-      <motion.div
-        className="bg-red-500 h-2 w-full fixed origin-left"
-        style={{
-          scaleX:scrollYProgress
-        }}
-        >
-
-        </motion.div>
-      <div className="h-screen px-80 p-20 text-center bg-[#111] ">
-        <h2 className="font-bold text-4xl font-momo m-5">
-          sheryians coding school
-        </h2>
-        <p className="font-normal text-xl ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, ex? Corporis ea sunt commodi! Provident expedita, maiores dolor hic, eligendi voluptates ipsa corrupti dolorem ex eos excepturi commodi magnam assumenda ea ullam praesentium a quas laudantium magni incidunt fugit possimus! Voluptas voluptatum sint optio, in perspiciatis error autem ad vero quis esse quaerat pariatur, voluptate corrupti aliquid deserunt quia repellendus. Distinctio a omnis ut iusto vitae, harum dolorem quas! Ad enim omnis dolor sint qui perferendis! Eveniet quis rerum quam. Rem enim provident maxime minus nulla quod delectus. Corporis ipsa iusto doloribus assumenda, vel natus molestias quod cum. Ex, minima.
-        </p>
-      </div>
-           <div className="h-screen px-80 p-20 text-center bg-[#111] ">
-        <h2 className="font-bold text-4xl font-momo m-5">
-          sheryians coding school
-        </h2>
-        <p className="font-normal text-xl ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, ex? Corporis ea sunt commodi! Provident expedita, maiores dolor hic, eligendi voluptates ipsa corrupti dolorem ex eos excepturi commodi magnam assumenda ea ullam praesentium a quas laudantium magni incidunt fugit possimus! Voluptas voluptatum sint optio, in perspiciatis error autem ad vero quis esse quaerat pariatur, voluptate corrupti aliquid deserunt quia repellendus. Distinctio a omnis ut iusto vitae, harum dolorem quas! Ad enim omnis dolor sint qui perferendis! Eveniet quis rerum quam. Rem enim provident maxime minus nulla quod delectus. Corporis ipsa iusto doloribus assumenda, vel natus molestias quod cum. Ex, minima.
-        </p>
-      </div>
-           <div className="h-screen px-80 p-20 text-center bg-[#111] ">
-        <h2 className="font-bold text-4xl font-momo m-5">
-          sheryians coding school
-        </h2>
-        <p className="font-normal text-xl ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, ex? Corporis ea sunt commodi! Provident expedita, maiores dolor hic, eligendi voluptates ipsa corrupti dolorem ex eos excepturi commodi magnam assumenda ea ullam praesentium a quas laudantium magni incidunt fugit possimus! Voluptas voluptatum sint optio, in perspiciatis error autem ad vero quis esse quaerat pariatur, voluptate corrupti aliquid deserunt quia repellendus. Distinctio a omnis ut iusto vitae, harum dolorem quas! Ad enim omnis dolor sint qui perferendis! Eveniet quis rerum quam. Rem enim provident maxime minus nulla quod delectus. Corporis ipsa iusto doloribus assumenda, vel natus molestias quod cum. Ex, minima.
-        </p>
-      </div>
-           <div className="h-screen px-80 p-20 text-center bg-[#111] ">
-        <h2 className="font-bold text-4xl font-momo m-5">
-          sheryians coding school
-        </h2>
-        <p className="font-normal text-xl ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, ex? Corporis ea sunt commodi! Provident expedita, maiores dolor hic, eligendi voluptates ipsa corrupti dolorem ex eos excepturi commodi magnam assumenda ea ullam praesentium a quas laudantium magni incidunt fugit possimus! Voluptas voluptatum sint optio, in perspiciatis error autem ad vero quis esse quaerat pariatur, voluptate corrupti aliquid deserunt quia repellendus. Distinctio a omnis ut iusto vitae, harum dolorem quas! Ad enim omnis dolor sint qui perferendis! Eveniet quis rerum quam. Rem enim provident maxime minus nulla quod delectus. Corporis ipsa iusto doloribus assumenda, vel natus molestias quod cum. Ex, minima.
-        </p>
-      </div>
-    </div>
+  const btnClicking=() =>{
+    setBtnClicked(!btnClicked);
+    console.log(btnClicked);
+  }
+  return(
+ <div className='bg-[#111] h-screen p-10'>
+ <AnimatePresence>
+   <motion.div
+  layout
+  className='h-60 w-60 rounded-2xl bg-red-500'
+    style={{
+      height:btnClicked?500:100,
+      width:btnClicked?500:100
+    }}
+    >
+  </motion.div>
+  <button onClick={btnClicking} className='bg-green-700 mt-4 active:scale-95 font-bold rounded px-6 py-2'>click</button>
+ </AnimatePresence>
+ </div>
   )
 }
-
 export default App;
